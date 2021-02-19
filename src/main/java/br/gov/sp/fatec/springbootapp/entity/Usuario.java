0e6edc344d3600902ad68227bd27fea95c1909e0
rodrigocr16 <rodrigo.reis13@fatec.sp.gov.br>
2021-02-19 23:59:29 +0000

    aula 03 - 'JPA 1'

---
+++ src/main/java/br/gov/sp/fatec/springbootapp/entity/Usuario.java
@@ -0,0 +1,56 @@
+package br.gov.sp.fatec.springbootapp.entity;
+
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+@Entity
+@Table(name = "usu_usuario")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "usu_id")
+    private Long id;
+
+    @Column(name = "usu_nome_usuario")
+    private String nomeUsuario;
+
+    @Column(name = "usu_senha")
+    private String senha;
+
+    @Column(name = "usu_nome_exibicao")
+    private String nomeExibicao;
+
+
+    public Long getId() {
+        return this.id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+    public void setNomeUsuario(String nomeUsuario) {
+        this.nomeUsuario = nomeUsuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getNomeExibicao() {
+        return nomeExibicao;
+    }
+    public void setNomeExibicao(String nomeExibicao) {
+        this.nomeExibicao = nomeExibicao;
+    }
+}
--- src/test/java/br/gov/sp/fatec/springbootapp/SpringBootAppApplicationTests.java
+++ src/test/java/br/gov/sp/fatec/springbootapp/SpringBootAppApplicationTests.java
@@ -1,13 +1,35 @@
 package br.gov.sp.fatec.springbootapp;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
+import br.gov.sp.fatec.springbootapp.entity.Usuario;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import br.gov.sp.fatec.springbootapp.repository.UsuarioRepository;
 
 @SpringBootTest
+@Transactional
+@Rollback
 class SpringBootAppApplicationTests {
+
+    @Autowired
+    private UsuarioRepository usuarioRepo;
 
 	@Test
 	void contextLoads() {
-	}
+    }
 
+    @Test
+    void testaInsercao() {
+        Usuario usuario = new Usuario();
+        usuario.setNomeUsuario("usuario_teste");
+        usuario.setSenha("senha");
+        usuario.setNomeExibicao("teste");
+
+        usuarioRepo.save(usuario);
+        assertNotNull(usuario.getId());
+    }
 }
