610de205614d92d7f8a9a54e78e24951c94a57b5
rodrigocr16 <rodrigo.reis13@fatec.sp.gov.br>
2021-02-25 22:18:45 +0000

    aula 05 - 'JPA 3'

--- src/main/java/br/gov/sp/fatec/springbootapp/repository/UsuarioRepository.java
+++ src/main/java/br/gov/sp/fatec/springbootapp/repository/UsuarioRepository.java
@@ -1,9 +1,16 @@
 package br.gov.sp.fatec.springbootapp.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
+import java.util.List;
 import br.gov.sp.fatec.springbootapp.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
     
+    public List<Usuario> findUsuarioByNomeExibicaoContainsIgnoreCase(String nomeExibicao);
+
+    public Usuario findUsuarioByNomeExibicaoOrNomeUsuarioContainsIgnoreCase(String nomeExibicao, String nomeUsuario);
+
+    public List<Usuario> findUsuarioByAutorizacoesTipo(String autorizacao);
+
+    public Usuario findUsuarioByNomeUsuarioAndSenha(String nomeUsuario, String senha);
 }
--- src/test/java/br/gov/sp/fatec/springbootapp/SpringBootAppApplicationTests.java
+++ src/test/java/br/gov/sp/fatec/springbootapp/SpringBootAppApplicationTests.java
@@ -3,18 +3,18 @@ package br.gov.sp.fatec.springbootapp;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.HashSet;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
-import br.gov.sp.fatec.springbootapp.entity.Autorizacao;
 import br.gov.sp.fatec.springbootapp.entity.Usuario;
 import org.springframework.test.annotation.Rollback;
+import br.gov.sp.fatec.springbootapp.entity.Autorizacao;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-
-import br.gov.sp.fatec.springbootapp.repository.AutorizacaoRepository;
 import br.gov.sp.fatec.springbootapp.repository.UsuarioRepository;
+import br.gov.sp.fatec.springbootapp.repository.AutorizacaoRepository;
 
 @SpringBootTest
 @Transactional
@@ -57,6 +57,22 @@ class SpringBootAppApplicationTests {
     void testaUsuario() {        
         Autorizacao aut = autRepo.findById(1L).get();
         assertEquals("rodrigocr16", aut.getUsuarios().iterator().next().getNomeUsuario());
+    }
+
+    @Test
+    void testaBuscaUsuarioNomeContains() {
+        List<Usuario> usuarios = usuarioRepo.findUsuarioByNomeExibicaoContainsIgnoreCase("RoDrIgO");
+        assertTrue(!usuarios.isEmpty());
+    }
+    @Test
+    void testaBuscaUsuarioNomeSenha() {
+        Usuario usuario = usuarioRepo.findUsuarioByNomeUsuarioAndSenha("rodrigocr16", "pepino");
+        assertNotNull(usuario);
+    }
+    @Test
+    void testaBuscaUsuarioAutorizacao() {
+        List<Usuario> usuarios = usuarioRepo.findUsuarioByAutorizacoesTipo("admin");
+        assertTrue(!usuarios.isEmpty());
     }
 
 
