344bcd928a0d9f9bebb346f10fdbbc301e6bf3b6
rodrigocr16 <rodrigo.reis13@fatec.sp.gov.br>
2021-03-05 23:51:24 +0000

    wip

--- src/main/java/br/gov/sp/fatec/springbootapp/SpringBootAppApplication.java
+++ src/main/java/br/gov/sp/fatec/springbootapp/SpringBootAppApplication.java
@@ -1,13 +1,29 @@
 package br.gov.sp.fatec.springbootapp;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.beans.factory.annotation.Autowired;
+import br.gov.sp.fatec.springbootapp.repository.ClasseRepository;
+import br.gov.sp.fatec.springbootapp.repository.UsuarioRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class SpringBootAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootAppApplication.class, args);
-	}
+    @Autowired
+    private UsuarioRepository usuarioRepo;
 
+    @Autowired
+    private ClasseRepository classeRepo;
+
+	public static void main(String[] args) {
+        SpringApplication.run(SpringBootAppApplication.class, args);
+        
+        transacao();
+
+    }
+    
+    public static void transacao() {
+        System.out.print("Pau no cu dos prejudicados");
+    }
 }
